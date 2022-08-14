6542112ea5ca818ca376214225c8208003135c10
Babin Bohora <bohorababin1@gmail.com>
2022-08-14 10:50:55 +0900

    ground work for nav_bar:

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :features, :pricing ]
 
   def home
+  end
+
+  def features
+  end
+
+  def pricing
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get "/features", to: "pages#features"
+  get "/pricing", to: "pages#pricing"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
