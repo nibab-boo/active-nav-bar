3a925a5d84f87ab73a2d7fb733728fb82c137971
Babin Bohora <bohorababin1@gmail.com>
2022-08-14 12:11:37 +0900

    Created current_page helper_method and refactored nav component

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  # Defining we are going to use current_path as helper_method
+  helper_method :current_path
+
+  private
+
+  # It is always a good idea to keep everything private.
+  def current_path path
+    request.path == path ? "active" : ""
+  end
 end
